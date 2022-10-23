247e67c2a9bf16c8986674892d5c8d45b80eb7af
Dashinmu <90204594+Dashinmu@users.noreply.github.com>
2022-10-23 20:07:47 +0500

    add lab6_v1.0

---
+++ С#/WindowsFormLab6/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MainShared;
+
+namespace WindowsFormLab6
+{
+    public partial class Form1 : Form
+    {
+        SharedClass.RectangleD func1 = SharedClass.SquareRectangle;
+        SharedClass.RectangleD func2 = SharedClass.PerimeterRectangle;
+
+        int x, y, res_square, res_perimeter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToInt32(textBox1.Text);
+            y = Convert.ToInt32(textBox2.Text);
+            res_square = func1(x, y);
+            res_perimeter = func2(x, y);
+            textBox3.Text = res_square + " & " + res_perimeter;
+        }
+    }
+}
