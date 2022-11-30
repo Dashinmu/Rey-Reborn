6ffccdc38852560468c2654ea8bd6c7a3f5d5b80
Dashinmu <90204594+Dashinmu@users.noreply.github.com>
2022-11-30 12:31:51 +0500

    add lab9_v1.0

---
+++ С#/Lab9/WPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<PersonClass> Persons;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Persons = new ObservableCollection<PersonClass>();
+            Persons.Add(new PersonClass("Ivanov", "Ivan", "Ivanich", 25));
+            Persons.Add(new PersonClass("Kabanov", "KaBan", "Kabanovich", 22));
+            UsersList.DataContext = Persons;
+        }
+
+        private void ViewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2(UsersList.SelectedItem, false);
+            window2.Owner = this;
+            window2.ShowDialog();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PersonClass person = new PersonClass();
+            Window2 window2 = new Window2(person, true);
+            window2.Owner = this;
+            window2.ShowDialog();
+
+            if (person.Age != 0)
+            {
+                Persons.Add(person);
+            }
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2(UsersList.SelectedItem, true);
+            window2.Owner = this;
+            window2.ShowDialog();
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PersonClass person = UsersList.SelectedItem as PersonClass;
+            string str = string.Format("Delete note: {0} {1}", person.LastName, person.FirstName);
+            if (MessageBox.Show(this, str, "Warning", MessageBoxButton.YesNo) == MessageBoxResult.Yes) Persons.Remove(person);
+        }
+    }
+}
---
+++ С#/Lab9/WPF/PersonClass.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF
+{
+    public class PersonClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+        private string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set 
+            {
+                if (value != _FirstName)
+                {
+                    _FirstName = value;
+                    OnPropertyChanged("FirstName");
+                }
+            }
+        }
+
+        private string _LastName;
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (value != _LastName)
+                {
+                    _LastName = value;
+                    OnPropertyChanged("LastName");
+                }
+            }
+        }
+
+        private string _SecondName;
+        public string SecondName
+        {
+            get { return _SecondName; }
+            set
+            {
+                if (value != _SecondName)
+                {
+                   _SecondName = value;
+                    OnPropertyChanged("SecondName");
+                }
+            }
+        }
+
+        private int _Age;
+        public int Age
+        {
+            get { return _Age; }
+            set
+            {
+                if (value != _Age)
+                {
+                    _Age = value;
+                    OnPropertyChanged("Age");
+                }
+            }
+        }
+
+        public PersonClass() : this("FirstName", "LastName", "SecondName", 0) { }
+        public PersonClass(string ln, string fn, string sn, int yo)
+        {
+            LastName = ln;
+            FirstName = fn;
+            SecondName = sn;
+            Age = yo;
+        }
+
+        public PersonClass(PersonClass person) : this (person.LastName, person.FirstName, person.SecondName, person.Age) { }
+
+        public static void CopyData(PersonClass person1, PersonClass person2)
+        {
+            person1.LastName = person2.LastName;
+            person1.FirstName = person2.FirstName;
+            person1.SecondName = person2.SecondName;
+            person1.Age = person2.Age;
+        }
+    }
+}
